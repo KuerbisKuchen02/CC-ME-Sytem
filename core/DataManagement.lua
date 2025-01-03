5573f947521b6a78d3064bba606c66d7e9b803f0
josiamenger <josia.menger@gmx.de>
2025-01-03 15:37:50 +0100

    Add persisting to DataManagement class

--- core/DataManagement.lua
+++ core/DataManagement.lua
@@ -2,22 +2,28 @@
 --- Support for selecting, inserting, updating and deleting objects and indices for faster lookups
 
 local c = require("libs.class")
+local Persist = require("libs.persist")
 
 
 --- Create an index for a table
 ---
 --- @param table table table to create the index for
---- @param key any key to index by
-local function createIndex (table, key)
-    local index = {}
+--- @param keys table keys to create the index for
+local function createIndex (table, keys)
+    assert(type(table) == "table", "Table must be a table")
+    assert(type(keys) == "table", "Keys must be a table")
+
+    local indices = {}
     for k, v in pairs(table) do
-        local value = v[key]
-        if value then
-            index[value] = index[value] or {}
-            table.insert(index[value], v)
+        for _, key in ipairs(keys) do
+            local value = v[key]
+            if value then
+                indices[key] = indices[key] or {}
+                table.insert(indices[key], v)
+            end
         end
     end
-    return index
+    return indices
 end
 
 
@@ -26,6 +32,9 @@ end
 --- @param indices table indices to insert the object into
 --- @param obj table object to insert
 local function insertInIndices (indices, obj)
+    assert(type(indices) == "table", "Indices must be a table")
+    assert(type(obj) == "table", "Object must be a table")
+
     for key, index in pairs(indices) do
         local value = obj[key]
         index[value] = index[value] or {}
@@ -39,6 +48,9 @@ end
 --- @param indices table indices to remove the object from
 --- @param obj table object to remove
 local function removeFromIndices (indices, obj)
+    assert(type(indices) == "table", "Indices must be a table")
+    assert(type(obj) == "table", "Object must be a table")
+
     for key, index in pairs(indices) do
         local objects = index[obj[key]]
         for i, v in ipairs(objects) do
@@ -51,29 +63,51 @@ local function removeFromIndices (indices, obj)
 end
 
 
+--- Persist an object
+---
+---@param obj DataManagement object to persist
+local function persist(obj)
+    obj._meta.changes = obj._meta.changes + 1
+    if obj._meta.changes > 10 then
+        obj:save()
+        obj._meta.changes = 0
+    end
+end
+
+
 --- Abstract class for managing data
 --- @class DataManagement 
-local DataManagement = c.class()
+local DataManagement = c.class(Persist)
 
 --- Constructor for DataManagement
 ---
---- Initializes the data and indices tables
-function DataManagement:constructor ()
-    self._data = {}
-    self._indices = {}
+--- @param indexed_keys? table keys to index
+--- @param filename? string filename to save the data to
+function DataManagement:constructor (indexed_keys, filename)
+    if not filename and type(indexed_keys) == "string" then
+        filename = indexed_keys
+        indexed_keys = nil
+    end
+    self:super("constructor", filename or "db")
+    if type(indexed_keys) == "table" then
+        self._indices = createIndex(self._data, indexed_keys)
+    else 
+        self._indices = {}
+    end
+    self._meta = {changes = 0}
 end
 
 --- Selects all objects from the data table that match the given predicate
 ---
---- @param value any value to match
---- @param key any key to match or nil to match by id
+--- @param key any key or id to match
+--- @param value? any value to match or nil to match by id
 --- @return table objects that match the value
-function DataManagement:select (value, key)
+function DataManagement:select (key, value)
     if key == nil and value == nil then
         return self._data
     end
-    if key == nil then
-        return {self._data[value]}
+    if value == nil then
+        return {self._data[key]}
     end
     local index = self._indices[key]
     if index then
@@ -90,12 +124,12 @@ end
 
 --- Selects the first object from the data table that matches the given predicate
 ---
---- @param value any value to match or id to match by id
---- @param key any key to match or nil to match by id
+--- @param key any key or id to match
+--- @param value? any value to match or nil to match by id
 --- @return table|nil object that matches the value
-function DataManagement:selectOne (value, key)
-    if key == nil then
-        return self._data[value]
+function DataManagement:selectOne (key, value)
+    if value == nil then
+        return self._data[key]
     end
     local index = self._indices[key]
     if index then
@@ -117,8 +151,10 @@ end
 --- Inserts an object into the data table
 ---
 --- @param obj table object to insert
---- @param id any optional id to insert the object at a specific index
+--- @param id? any optional id to insert the object at a specific index
 function DataManagement:insert (obj, id)
+    assert(type(obj) == "table", "Object must be a table")
+
     if id then
         if self._data[id] then
             error("Object with id " .. id .. " already exists")
@@ -131,6 +167,7 @@ function DataManagement:insert (obj, id)
         table.insert(self._data, obj)
     end
     insertInIndices(self._indices, obj)
+    persist(self)
 end
 
 --- Updates an object in the data table
@@ -138,23 +175,31 @@ end
 --- @param id any id of the object to update
 --- @param obj table object to update
 function DataManagement:update (id, obj)
+    assert(type(id) ~= "nil", "Id must not be nil")
+    assert(type(obj) == "table", "Object must be a table")
+
     if not self._data[id] then
         error("Object with id " .. id .. " does not exist")
     end
     for key, value in pairs(obj) do
         self._data[id][key] = value
     end
+    persist(self)
 end
 
 --- Deletes an object from the data table
 ---
 --- @param id any id of the object to delete
 function DataManagement:delete (id)
+    assert(type(id) ~= "nil", "Id must not be nil")
+
     if not self._data[id] then
         error("Object with id " .. id .. " does not exist")
     end
     removeFromIndices(self._indices, self._data[id])
     self._data[id] = nil
+    persist(self)
 end
+
 
 return DataManagement
