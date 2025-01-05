72d3b53ceffdd64ae78861bab453a359b79ac15e
josiamenger <josia.menger@gmx.de>
2025-01-05 19:33:05 +0100

    Add inventory model and repository

---
+++ server/src/models/Inventory.lua
@@ -0,0 +1,27 @@
+local c = require("libs.class")
+local Data = require("server.src.models.Data")
+
+--- Class representing an inventory
+--- @class Inventory: Data
+local Inventory = c.class(Data)
+
+--- Create a new Inventory object
+--- @param displayName string|nil the display name of the inventory
+--- @param inventoryType string|nil the type of the inventory
+--- @param itemName string|nil the item id of the inventory
+function Inventory:constructor (displayName, inventoryType, itemName)
+    assert(type(inventoryType) == nil
+        or inventoryType == "mixed"
+        or inventoryType == "bulk" 
+        or inventoryType == "input"
+        or inventoryType == "output",
+        "inventoryType must be nil, 'mixed', 'bulk', 'input' or 'output'"
+    )
+
+    self:super()
+    self.displayName = displayName
+    self.inventoryType = inventoryType or "mixed"
+    self.itemName = itemName
+end
+
+return Inventory
